b5df141340fd5f734187605747e457cb25ee0c0d
Bminor87 <jessehellman@gmail.com>
2025-02-10 19:24:51 +0200

    Add notifications when matched

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AppNavigator from "./navigation/AppNavigator";
 import { MatchProvider } from "./contexts/MatchContext";
+import Toast from "react-native-toast-message";
 
 export default function App() {
   return (
     <MatchProvider>
       <AppNavigator />
+      <Toast />
     </MatchProvider>
   );
 }
