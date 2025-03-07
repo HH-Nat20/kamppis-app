e95a0ad83a8ecca7be5c52450ad561759a16bf1a
Hellman Studios <jesse.hellman@hellmanstudios.fi>
2025-03-07 12:33:55 +0200

    Add profile details page

--- app/ui/colors.ts
+++ app/ui/colors.ts
@@ -4,11 +4,13 @@ const colors = {
     tertiary: "#233FA6",
     quatenary: "#020D36",
     quinary: "#22AB49",
-    senary: "#8FFFAF", // #2C944A
+    senary: "#8FFFAF",
     septenary: "#CC0E96",
     octonary: "#692054",
     nonary: "#C7A21E",
     denary: "#F5CB36",
+    extra: "#F96E46",
+    bonus: "#A18276",
 
     success: "#22AB49",
     warning: "#C7A21E",
@@ -27,6 +29,15 @@ const colors = {
     active: "#8FFFAF",
     inactive: "#8F8F8F",
     disabled: "#293358",
+
+    electricBlue: "#00E8FC",
+    mistyRose: "#FFE3E3",
+    silver: "A3A3A3",
+    battleshipGrey: "#7A918D",
+    green: "#2C944A",
+    tomato: "#FE654F",
+    crayolaRed: "#E84855",
+
 }
 
 export const bgGradient: [string, string, ...string[]] = [
@@ -47,7 +58,7 @@ export const renderTagBgColor = (tag: string): string => {
         case "STUDENT":
             return colors.quinary;
         case "WORKING":
-            return colors.quatenary;
+            return colors.extra;
         case "PARTY_GOER":
             return colors.octonary;
         case "EARLY_BIRD":
