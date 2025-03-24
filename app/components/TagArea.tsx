c89f9c6a592d54b6543e35be3326fa038cb850ae
Hellman Studios <jesse.hellman@hellmanstudios.fi>
2025-03-24 19:50:40 +0200

    Add icons for flat utilities

--- app/components/TagArea.tsx
+++ app/components/TagArea.tsx
@@ -4,14 +4,50 @@ import { ProfileCard } from "../types/ProfileCard";
 import styles from "../ui/styles";
 import { renderTagBgColor } from "../ui/colors";
 
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
 const TagArea: React.FC<{ card: ProfileCard }> = ({ card }) => {
   const tags: string[] = [];
+
+  const profileType = card?.profile?.type;
+
   if (card.profile && "lifestyle" in card.profile) {
     tags.push(...card.profile.lifestyle);
   }
   if (card.profile && "flat" in card.profile && card.profile.flat) {
     tags.push(...card.profile.flat.flatUtilities);
   }
+
+  const renderTag = (tag: string) => {
+    switch (tag) {
+      case "PRIVATE_BATHROOM":
+        return <MaterialIcons name="bathtub" size={24} color="white" />;
+      case "SEPARATE_BATHROOM_AND_SHOWER":
+        return <FontAwesome name="bathtub" size={24} color="white" />;
+      case "BALCONY":
+        return <MaterialIcons name="balcony" size={24} color="white" />;
+      case "WIFI":
+        return <AntDesign name="wifi" size={24} color="white" />;
+      case "DISHWASHER":
+        return (
+          <MaterialCommunityIcons name="dishwasher" size={24} color="white" />
+        );
+      case "LAUNDRY_MACHINE":
+        return (
+          <MaterialCommunityIcons
+            name="washing-machine"
+            size={24}
+            color="white"
+          />
+        );
+      default:
+        return tag.replace("_", " ");
+    }
+  };
+
   return (
     <View style={styles.tagArea}>
       {tags.map((tag, index) => (
@@ -22,7 +58,7 @@ const TagArea: React.FC<{ card: ProfileCard }> = ({ card }) => {
             backgroundColor: renderTagBgColor(tag),
           }}
         >
-          {tag.replaceAll("_", " ")}
+          {renderTag(tag)}
         </Text>
       ))}
     </View>
