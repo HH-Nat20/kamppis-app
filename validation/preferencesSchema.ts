a14f0e4f581c2619ef794286e4db9f758d449a16
Hellman Studios <jesse.hellman@hellmanstudios.fi>
2025-04-28 21:06:17 +0300

    #21 Users can no longer select a max age less than min age

--- validation/preferencesSchema.ts
+++ validation/preferencesSchema.ts
@@ -2,13 +2,24 @@ import { z } from "zod";
 import { Gender } from "@/types/enums/GenderEnum";
 import { Location } from "@/types/enums/LocationEnum";
 
-export const preferencesSchema = z.object({
-  maxRent: z.number().min(0),
-  hasPrivateRoom: z.boolean().nullable(),
-  maxRoommates: z.number().min(1).max(10),
-  minAgePreference: z.number().min(18),
-  maxAgePreference: z.number().max(120),
-  genderPreferences: z.array(z.nativeEnum(Gender)),
-  locationPreferences: z.array(z.nativeEnum(Location)),
-});
+export const preferencesSchema = z
+  .object({
+    maxRent: z.number().min(0),
+    hasPrivateRoom: z.boolean().nullable(),
+    maxRoommates: z.number().min(1).max(10),
+    minAgePreference: z.number().min(18),
+    maxAgePreference: z.number().max(120),
+    genderPreferences: z.array(z.nativeEnum(Gender)),
+    locationPreferences: z.array(z.nativeEnum(Location)),
+  })
+  .refine(
+    (data) => {
+      return data.maxAgePreference >= data.minAgePreference;
+    },
+    {
+      message: "Max age must be greater than or equal to min age.",
+      path: ["maxAgePreference"],
+    }
+  );
+
 export type PreferencesForm = z.infer<typeof preferencesSchema>;
